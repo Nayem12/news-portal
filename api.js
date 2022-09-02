ce1d586d265116030423e130f54245b0b745a4c4
Nayem 12 <shanaeem773@gmail.com>
2022-09-02 21:38:12 +0600

    categoirs-added

--- api.js
+++ api.js
@@ -1,1 +1,22 @@
-console.log('js file added')
+const Allcategories = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => addCategories(data.data.news_category))
+}
+const addCategories = categories => {
+    categories.forEach(categorie => {
+        console.log(categorie);
+        const addcategoriesId = document.getElementById('add-categories');
+        const creatdiv = document.createElement('div');
+        creatdiv.classList.add('col');
+        creatdiv.innerHTML = `
+            <div class="col single-style">
+                 <button class="btn">${categorie.category_name}</button>
+            </div>
+        `
+        addcategoriesId.appendChild(creatdiv);
+
+    })
+
+}
+Allcategories()
