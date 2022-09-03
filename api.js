9bfe8c89a7e3938f1b4f36e3be6c34e5986fcfa8
Nayem 12 <shanaeem773@gmail.com>
2022-09-03 12:48:49 +0600

    how many news added

--- api.js
+++ api.js
@@ -15,7 +15,7 @@ const addCategories = categories => {
             </div>
         `
         addcategoriesId.appendChild(creatdiv);
-        togglebar(true)
+        // togglebar(true)
     })
     // '${categorie}'
 }
@@ -28,7 +28,7 @@ const newsfile = (category_id) => {
 }
 
 const singlenews = finds => {
-
+    const showNews = document.getElementById('show-all-news').innerText = finds.length;
     const newssection = document.getElementById('news-section');
     if (finds.length === 0) {
         alert('No news found')
@@ -39,19 +39,26 @@ const singlenews = finds => {
         const CreatDiv = document.createElement('div');
         CreatDiv.classList.add('row', 'g-0', 'mt-5');
         CreatDiv.innerHTML = `
-    <div class="col-lg-4 col-md-12">
+    <div class="col-lg-4 col-md-12" data-bs-toggle="modal" data-bs-target="#searchOnDetails" onclick="personalId('${find._id}')">
         <img src="${find.image_url}" class="img-fluid rounded-start" alt="...">
     </div>
-    <div class="col-lg-8 col-md-12">
+    <div class="col-lg-8 col-md-12" data-bs-toggle="modal" data-bs-target="#searchOnDetails" onclick="personalId('${find._id}')">
         <div class="card-body ms-2">
             <h5 class="card-title">${find.title}</h5>
-            <p class="card-text" id="news-details">Hlw</p>
+            <p class="card-text" id="news-details">${find.details ? find.details.slice(0, 80) : find.details}</p>
             <div id="author-visit">
-                <div>
-                    <p>${find.author.name ? find.author.name : 'nothing found'}</p>
-                    <p>${find.author.published_date}</p>
+                <div class="author-info">
+                    <div class="w-25 h-25">
+                        <img src="${find.author.img}" class="img-fluid rounded-circle w-25 h-25" alt="...">
+                    </div>
+                    <div>
+                        <p>${find.author.name ? find.author.name : 'nothing found'}</p>
+                        <p>${find.author.published_date}</p>
+                    </div>
                 </div>
-                <div><p>views:${find.total_view ? find.total_view : 'nothing found'}</p></div>
+                <div>
+                    <p>views:${find.total_view ? find.total_view : 'nothing found'}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -75,6 +82,13 @@ const togglebar = isloading => {
         loadingbar.classList.add('d-none')
     }
 }
+const personalId = news_id => {
+    const url = `https://openapi.programming-hero.com/api/news/${news_id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+personalId();
 
 Allcategories()
 newsfile('05');
